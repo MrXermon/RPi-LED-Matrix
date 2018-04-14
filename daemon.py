80362c5f98d953acf63883cf90506011063100d2
Xermon <you@example.com>
2018-04-14 12:47:05 +0200

    Process Priority

--- daemon.py
+++ daemon.py
@@ -1,6 +1,7 @@
 import MySQLdb
 import sys
 import time
+from PIL import ImageFont
 
 import configuration
 
@@ -9,7 +10,8 @@ from luma.core.interface.serial import spi, noop
 from luma.core.render import canvas
 from luma.core.virtual import viewport
 from luma.led_matrix.device import max7219
-from luma.core.legacy.font import proportional, LCD_FONT
+from luma.core.legacy import text, show_message
+from luma.core.legacy.font import proportional, CP437_FONT, TINY_FONT, SINCLAIR_FONT, LCD_FONT
 
 # LED Matrix Setup
 serial = spi(port=0, device=0, gpio=noop())
@@ -17,6 +19,14 @@ device = max7219(serial, cascaded=12, block_orientation=-90)
 
 device.contrast(10)
 device.clear()
+
+# Wait until boot finished
+time.sleep(30)
+
+# Show Startup Messages
+if len(configuration.STARTUP_MESSAGE):
+	for m in configuration.STARTUP_MESSAGE:
+		show_message(device, m, fill="white", font=proportional(CP437_FONT),scroll_delay=0.02)
 
 # Setup DB connection
 SQLcon = MySQLdb.connect(configuration.DATABASE['HOST'], configuration.DATABASE['USER'], configuration.DATABASE['PASSWORD'], configuration.DATABASE['NAME'])
@@ -31,7 +41,7 @@ with SQLcon:
 		SQLcur.execute('SET character_set_connection=utf8;')
 
 		# Grab unshown messages from DB
-		SQLcur.execute("SELECT `message_id`, `provider_name`, `message_text`, `message_duration`, `message_from` FROM `message` JOIN `provider` ON `message`.`provider_id` = `provider`.`provider_id` WHERE `message_shown` = 0 ORDER BY `message_id` ASC;")
+		SQLcur.execute("SELECT `message_id`, `provider_name`, `message_text`, `message_from` FROM `message` JOIN `provider` ON `message`.`provider_id` = `provider`.`provider_id` WHERE `message_shown` = 0 ORDER BY `message_id` ASC;")
 
 		# Grab one message after another
 		for i in range(SQLcur.rowcount):
@@ -44,15 +54,11 @@ with SQLcon:
 
 				# Prepare device + message
 				device.show()
-				message = SQLrow[2] + " // " + SQLrow[4] + " // " + SQLrow[1]
+				message = SQLrow[2] + " // " + SQLrow[3] + " // " + SQLrow[1]
 				print "Started showing message id=" + str(SQLrow[0]) + ", time=" + str(SQLrow[3]) + ", message=" + message
 
 				# Show message
-				with canvas(device) as draw:
-					draw.rectangle(device.bounding_box, outline="white")
-
-				# Wait
-				time.sleep(SQLrow[3])
+				show_message(device, message, fill="white", font=proportional(CP437_FONT),scroll_delay=0.02)
 
 				# Turn Off and clear device
 				device.hide()
--- start.sh
+++ start.sh
@@ -1,2 +1,3 @@
 cd /opt/led/
 /usr/bin/python2.7 daemon.py
+renice -n -15 -p $(pgrep python2.7)
