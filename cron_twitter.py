d466dde3b29e2e03ea8472ce6a8d7ec1139b499d
Xermon <you@example.com>
2018-04-28 09:50:52 +0200

    Twitter like and retweet only mentions.

--- cron_twitter.py
+++ cron_twitter.py
@@ -49,9 +49,6 @@ if configuration.TWITTER['ACTIVE'] is 'true':
 	                        if SQLcur.fetchone()[0] == 0:
         	                        # Insert message to database
 					SQLcur.execute("INSERT INTO `message` (`message_text`, `message_from`, `message_id_external`, `provider_id`) VALUES (%s, %s, %s, '2');", (HTMLParser.unescape(Status.text), Status.user.screen_name, str(Status.id_str)))
-					# Like & retweet
-	                                TWITTERapi.CreateFavorite(status=Status)
-        	                        TWITTERapi.PostRetweet(status_id=Status.id_str)
 
         # Commit SQL transaction
         SQLcon.commit()
