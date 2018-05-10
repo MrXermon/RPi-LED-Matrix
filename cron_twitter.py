16a235f49bd4d7822a4dc3f2d46ffcae87552311
Xermon <you@example.com>
2018-05-10 12:18:04 +0200

    Searching for Twitter Hashtags.

--- cron_twitter.py
+++ cron_twitter.py
@@ -24,7 +24,6 @@ if configuration.TWITTER['ACTIVE'] is 'true':
                       access_token_secret=configuration.TWITTER['A_SECRET'],
 		      sleep_on_rate_limit=True)
 
-	# Grab mentions and convert to array
 	if configuration.TWITTER['MENTIONS'] is 'true':
 		# Fetch Mentions
 		TwitterMetions = TWITTERapi.GetMentions(count=5)
@@ -38,7 +37,7 @@ if configuration.TWITTER['ACTIVE'] is 'true':
 				TWITTERapi.CreateFavorite(status=Status)
 				TWITTERapi.PostRetweet(status_id=Status.id_str)
 
-	if configuration.TWITTER['TWEETS'] is 'true':
+	if configuration.TWITTER['USERS'] is 'true':
 		# Loop through all Users
 		for User in configuration.TWITTER['TWEETS_USERS']:
 			# Fetch User Timeline
@@ -49,6 +48,18 @@ if configuration.TWITTER['ACTIVE'] is 'true':
 	                        if SQLcur.fetchone()[0] == 0:
         	                        # Insert message to database
 					SQLcur.execute("INSERT INTO `message` (`message_text`, `message_from`, `message_id_external`, `provider_id`) VALUES (%s, %s, %s, '2');", (HTMLParser.unescape(Status.text), Status.user.screen_name, str(Status.id_str)))
+
+        if configuration.TWITTER['HASHTAGS'] is 'true':
+                # Loop through all Users
+                for Hashtag in configuration.TWITTER['TWEETS_HASHTAGS']:
+                        # Fetch Tweets by Hashtag
+                        Tweets = TWITTERapi.GetSearch(term=Hashtag, result_type="recent", count=5)
+                        # Check if Statuses already shown and insert into db
+                        for Status in Tweets:
+                              	SQLcur.execute("SELECT COUNT(*) FROM `message` WHERE `provider_id` = 2 AND `message_id_external` = '" + str(Status.id_str) + "';")
+                                if SQLcur.fetchone()[0] == 0:
+                                        # Insert message to database
+                                        SQLcur.execute("INSERT INTO `message` (`message_text`, `message_from`, `message_id_external`, `provider_id`) VALUES (%s, %s, %s, '2');", (HTMLParser.unescape(Status.text), Status.user.screen_name, str(Status.id_str)))
 
         # Commit SQL transaction
         SQLcon.commit()
--- start.sh
+++ start.sh
@@ -1,2 +1,3 @@
 cd /opt/led/
+sleep 1m
 /usr/bin/nice -n -15 /usr/bin/python2.7 daemon.py
